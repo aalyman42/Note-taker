24c46fb02930a275b653807beb0e0ab3d2833e2c
aalyman42 <aalyman42@gmail.com>
2024-01-02 20:30:41 -0700

    added uuid to json objects

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const db = require("./db/db.json");
 const app = express();
-
+const { v4: uuidv4 } = require("uuid");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,31 +16,43 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.get("/api/notes", (req, res) => {
-  return res.json(db);
-});
-
 app.post("/api/notes", (req, res) => {
   const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    const noteString = JSON.stringify(newNote);
 
-  if (req.body) {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
       } else {
         const parsedData = JSON.parse(data);
-        parsedData.push(req.body);
+        parsedData.push(newNote);
 
-        const stringData = JSON.stringify(parsedData);
+        const stringData = JSON.stringify(parsedData, null, 4);
         fs.writeFile("./db/db.json", stringData, (err) =>
           err ? console.error(err) : console.info("success")
         );
-        res.json({
-          title,
-          text,
-        });
       }
     });
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting review");
   }
 });
+
+app.get("/api/notes", (req, res) => {
+  return res.json(db);
+});
+
 app.listen(3001, () => console.log("Now listening at http://localhost:3001"));
