bf93301eb5bef5fd413c40c1cfe55c7690607049
aalyman42 <aalyman42@gmail.com>
2024-01-02 21:00:59 -0700

    reset index.js to be exact match to starter code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -31,6 +31,9 @@ let activeNote = {};
 const getNotes = () =>
   fetch("/api/notes", {
     method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 
 const saveNote = (note) =>
